d44792742ac6c6876693ddff0e40e6178a40b7b8
Soumys56 <soumyabera56@gmail.com>
2024-01-12 02:20:52 +0530

    product add

--- models/product.js
+++ models/product.js
@@ -1,18 +1,15 @@
 const mongoose=require('mongoose');
+const variantSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    sku: { type: String, required: true, unique: true },
+    additionalCost: { type: Number, default: 0 },
+    stockCount: { type: Number, default: 0 },
+  });
 const productSchema=new mongoose.Schema({
-    productId:{
-        type:Number,
-        required:true,
-        unique:true
-    },
-    productName:{
-        type:String,
-        required:true
-    },
-    price:{
-        type:Number,
-        required:true
-    }
+    name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  variants:[variantSchema]
 })
 const Product=mongoose.model("product",productSchema);
 
--- router/admin/addproduct.js
+++ router/admin/addproduct.js
@@ -2,28 +2,25 @@
 const express=require('express');
 const router=express.Router();
 const product=require('../../models/product');
+//Add products
 router.post('/add',async (req,res)=>{
-    try{
-       const result= await product.create({
-            productId:req.body.productId,
-            productName:req.body.productName,
-            price:req.body.price
-        })
-        if(result){
-            return res.json("add successfully") ;
-        }
-        else{
-            console.log("product add not sucessfully created");
-        }
+   
+        try {
+            const { name, description, price, variants } = req.body;
+            const productlist = await product.create({ name, description, price, variants });
+            res.status(201).json("product added sucessfully");
+          } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Internal Server Error' });
+          }
 
 
-    }catch(err){
-        console.log(err);
-    }
+         
 
 
        
 })
+//delete products based on id
 router.get('/delete/:id',async (req,res)=>{
     try{
         const result=await product.findByIdAndDelete(req.params.id);
@@ -44,5 +41,67 @@ router.get('/delete/:id',async (req,res)=>{
 
        
 })
+
+//update product
+router.put('/update/:id',async(req,res)=>{
+    try {
+      
+       
+        const { name, description, price, variants } = req.body;
+        const updatedProduct = await product.findByIdAndUpdate(
+          req.params.id,
+          { name, description, price, variants },
+          { new: true }
+        );
+        if (!updatedProduct) {
+          return res.status(404).json( 'Product not found' );
+        }
+        res.status(200).json("update product sucessfully");
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+      }
+})
+
+//retrive product
+router.get('/getproduct',async(req,res)=>{
+
+    try{
+        const productList=await product.find();
+        if(productList){
+            res.status(200).json(productList);
+
+        }
+        else{
+
+            return res.status(404).json( 'Product not found' );
+        }
+           
+
+    }catch(err){
+        res.status(500).json({ error: 'Internal Server Error' });
+        
+    }
+})
+
+// Search products
+router.get('/search', async(req,res)=>{
+    try {
+        const { query } = req.query;
+        console.log(query)
+        //you can search using product name ,description and variant name
+        const searchResults = await product.find({
+          $or: [
+            { name: { $regex: query, $options: 'i' } },
+            { description: { $regex: query, $options: 'i' } },
+            { 'variants.name': { $regex: query, $options: 'i' } },
+          ],
+        });
+        res.status(200).json(searchResults);
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+      }
+});
 
 module.exports=router;
